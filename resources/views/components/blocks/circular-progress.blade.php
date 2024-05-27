e0e69170a8790ef8216402b0177176d358c19845
Marzio Perez <marzioperez@gmail.com>
2024-05-27 14:09:43 -0500

    Ajustes visuales

--- resources/views/components/blocks/circular-progress.blade.php
+++ resources/views/components/blocks/circular-progress.blade.php
@@ -7,15 +7,20 @@
 @endphp
 <div class="container mx-auto px-4 sm:px-6 lg:px-8 relative py-5">
     <div class="sm:grid grid-cols-12">
-        <div class="col-span-8">
+        <div class="col-span-8 sm:text-left text-center">
             <h3 class="text-white font-semibold text-2xl mb-3">{{$data['title']}}</h3>
             <p class="text-white mb-3">{{$data['sub_title']}}</p>
-            <div class="mt-5">
+            <div class="mt-5 sm:block hidden">
                 <a href="{{$data['url']}}" class="btn btn-primary">{{$data['text_button']}}</a>
             </div>
         </div>
-        <div class="col-span-4">
-            <div class="circular-progress" aria-valuenow="{{$progress}}" aria-valuemin="0" aria-valuemax="100" style="--value:{{$progress}}"></div>
+        <div class="col-span-4 flex sm:justify-start justify-center sm:py-0 py-3">
+            <div>
+                <div class="circular-progress" aria-valuenow="{{$progress}}" aria-valuemin="0" aria-valuemax="100" style="--value:{{$progress}}"></div>
+                <div class="mt-5 sm:hidden block">
+                    <a href="{{$data['url']}}" class="btn btn-primary">{{$data['text_button']}}</a>
+                </div>
+            </div>
         </div>
     </div>
 </div>
