565f3070691755d56094b6245c19fbd7278b61d8
Marzio Perez <marzioperez@gmail.com>
2024-06-02 01:36:45 -0500

    ajuste de bloque 2

--- resources/views/components/blocks/block-2.blade.php
+++ resources/views/components/blocks/block-2.blade.php
@@ -5,19 +5,11 @@
             <h4 class="text-secondary font-semibold sm:text-5xl text-4xl uppercase">{{$data['title']}}</h4>
             <h2 class="text-secondary font-bold sm:text-2xl text-xl uppercase">{{$data['sub_title']}}</h2>
         </div>
-
+        <div class="col-span-4 relative">
+            <img src="{{url('storage/web/' . $data['image'])}}" class="w-full sm:mb-0 mb-6" />
+        </div>
         <div class="col-span-8">
-            <div>
-                <div class="text-white mb-5">{!! $data['content'] !!}</div>
-                <div class="sm:block hidden">
-                    <button type="button" class="btn btn-secondary" x-on:click.prevent="$dispatch('open-modal', {name: 'modal-organization'})">{{$data['text_button']}}</button>
-                </div>
-            </div>
-        </div>
-        <div class="col-span-4 flex items-center relative">
-            <img src="{{url('storage/web/' . $data['image'])}}" class="w-full" />
-        </div>
-        <div class="sm:hidden flex justify-center sm:pt-0 pt-5">
+            <div class="custom-box-scrollbar">{!! $data['content'] !!}</div>
             <button type="button" class="btn btn-secondary" x-on:click.prevent="$dispatch('open-modal', {name: 'modal-organization'})">{{$data['text_button']}}</button>
         </div>
     </div>
