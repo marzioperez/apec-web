bd75f1c55dcc7b1fb7fe8cceff41d81b62c32aa7
Marzio Perez <marzioperez@gmail.com>
2024-06-02 14:24:02 -0500

    Update header.blade.php

--- resources/views/livewire/common/header.blade.php
+++ resources/views/livewire/common/header.blade.php
@@ -60,7 +60,7 @@
                                         </clipPath>
                                     </defs>
                                 </svg>
-                                <span class="flex ml-3 font-semibold text-gray"><span class="sm:block hidden">Hi,</span> {{$user_name}}</span>
+                                <div class="flex ml-3 font-semibold text-gray space-x-2"><span class="sm:block hidden">Hi,</span> <span>{{$user_name}}</span></div>
                                 <svg class="ml-1 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                                     <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
                                 </svg>
