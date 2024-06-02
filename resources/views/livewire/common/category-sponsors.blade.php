6740d4eedf8b242d3cdd77f92a804d208c0cbe08
Marzio Perez <marzioperez@gmail.com>
2024-06-02 14:30:35 -0500

    Update category-sponsors.blade.php

--- resources/views/livewire/common/category-sponsors.blade.php
+++ resources/views/livewire/common/category-sponsors.blade.php
@@ -13,7 +13,7 @@
                 <ul class="splide__list">
                     @foreach($sponsors as $page)
                         <li class="splide__slide">
-                            <div class="grid sm:grid-cols-center-4 grid-cols-2 sm:gap-12 gap-6">
+                            <div class="grid sm:grid-cols-center-4 grid-cols-center-2 sm:gap-12 gap-6">
                                 @foreach($page as $item)
                                     <div class="w-full cursor-pointer flex items-center" x-on:click.prevent="$dispatch('show-sponsor', {sponsor: {{json_encode($item)}} })">
                                         <img src="{{url('storage/web/' . $item['logo'])}}" class="object-center w-full" />
