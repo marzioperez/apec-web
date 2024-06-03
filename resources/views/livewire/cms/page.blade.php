ffe8732ffbc9f306aed6e155ccd58ffb87ac25bd
Marzio Perez <marzioperez@gmail.com>
2024-06-03 03:34:05 -0500

    Ajuste modal

--- resources/views/livewire/cms/page.blade.php
+++ resources/views/livewire/cms/page.blade.php
@@ -30,13 +30,15 @@
 
         @if($block['type'] === 'block-2')
             <x-blocks.block-2 :data="$block['data']" />
-                <x-modal name="modal-organization" bg="black !sm:p-0" classes="modal-book">
+                <x-modal name="modal-organization" bg="black !px-0 !py-0" classes="modal-book">
                     <x-slot:body>
                         <button type="button" class="absolute z-30 rounded-full top-3 right-3 text-white flex items-center justify-center" x-on:click="$dispatch('close-modal')">
                             <i class="fa-light fa-xmark text-lg"></i>
                         </button>
+                        <img src="{{asset('img/organization-mobile.png')}}" class="w-full sm:hidden block">
+
                         <div class="w-full sm:grid grid-cols-12">
-                            <div class="col-span-7 flex items-center justify-center sm:px-10 px-5">
+                            <div class="col-span-7 flex items-center justify-center sm:px-10 px-5 sm:pt-0 pt-6 sm:pb-0 pb-6">
                                 <div>
                                     <h3 class="text-secondary text-3xl mb-3 font-bold">{{$block['data']['pop_up_title']}}</h3>
                                     <div class="text-white text-sm">{!! $block['data']['pop_up_content'] !!}</div>
