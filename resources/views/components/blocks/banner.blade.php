dd9d501f38aa03f9dba0d121bef2368466f626fd
Marzio Perez <marzioperez@gmail.com>
2024-06-03 02:59:12 -0500

    Ajutes

--- resources/views/components/blocks/banner.blade.php
+++ resources/views/components/blocks/banner.blade.php
@@ -38,5 +38,5 @@
             @endif
         </div>
     </div>
-    <img src="{{asset('img/bg-banner-right.png')}}" class="absolute -bottom-1/3 right-0 w-[300px] opacity-40 z-[5]">
+    <img src="{{asset('img/bg-banner-right.png')}}" class="absolute sm:block hidden -bottom-1/3 right-0 w-[300px] opacity-40 z-[5]">
 </div>
