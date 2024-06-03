0861a82d568d907dad25dad0eb1d6bcec4316c4e
Marzio Perez <marzioperez@gmail.com>
2024-06-03 02:51:57 -0500

    Botones de progreso por estado

--- resources/views/components/blocks/circular-progress.blade.php
+++ resources/views/components/blocks/circular-progress.blade.php
@@ -10,8 +10,31 @@
 <div class="container mx-auto px-4 sm:px-6 lg:px-8 relative py-5 scroll-mt-[102px] relative z-10" id="{{$data['id']}}">
     <div class="sm:grid grid-cols-12">
         <div class="col-span-8 sm:text-left text-center">
-            <h3 class="text-white font-semibold text-2xl mb-3">{{$data['title']}}</h3>
-            <p class="text-white mb-3">{{$data['sub_title']}}</p>
+            @if(in_array($user['status'], [\App\Concerns\Enums\Status::CONFIRMED->value]))
+                <h3 class="text-white font-semibold text-2xl mb-3">How to  complete you registration?</h3>
+            @endif
+
+            @if(in_array($user['type'], [
+                \App\Concerns\Enums\Types::PARTICIPANT->value,
+                \App\Concerns\Enums\Types::STAFF->value,
+                \App\Concerns\Enums\Types::COMPANION->value,
+                \App\Concerns\Enums\Types::VIP->value,
+            ]))
+                @if(in_array($user['status'], [\App\Concerns\Enums\Status::UNPAID->value]))
+                    <h3 class="text-white font-semibold text-2xl mb-3">To continue the process please complete your payment</h3>
+               @endif
+            @endif
+
+            @if(in_array($user['status'], [
+                \App\Concerns\Enums\Status::PENDING_APPROVAL_DATA->value,
+                \App\Concerns\Enums\Status::SEND_TO_CHANCELLERY->value,
+                \App\Concerns\Enums\Status::FINISHED->value,
+                \App\Concerns\Enums\Status::PENDING_CORRECT_DATA->value
+            ]))
+                <h3 class="text-white font-semibold text-2xl mb-3">Want to use our complimentary shuttle service?</h3>
+                <p class="text-white">Don’t forget to let us know your travel and accommodation arrangements</p>
+            @endif
+
             <div class="sm:flex hidden justify-center mt-5 space-x-6">
                 @if(in_array($user['status'], [
                     \App\Concerns\Enums\Status::CONFIRMED->value
@@ -35,7 +58,7 @@
                     \App\Concerns\Enums\Types::VIP->value,
                 ]))
                     @if(in_array($user['status'], [\App\Concerns\Enums\Status::UNPAID->value]))
-                        <a href="{{route('hotel')}}" class="btn btn-primary">Complete your payment</a>
+                        <a href="{{route('progress')}}" class="btn btn-primary">Complete your payment</a>
                     @endif
                 @endif
 
--- resources/views/components/blocks/progress.blade.php
+++ resources/views/components/blocks/progress.blade.php
@@ -14,13 +14,32 @@
             return ((val-min)/(max-min))*100
         }
     }"
-     class="container mx-auto px-4 sm:px-6 lg:px-8 relative py-5 scroll-mt-[102px] relative z-10" id="{{$data['id']}}">
-    @if($user['register_progress'] < 100)
-        <h3 class="text-white font-semibold text-2xl text-center mb-3">{{$data['title']}}</h3>
-        <p class="text-white mb-3 text-center">{{str_replace("#progress#", $user['register_progress'], $data['sub_title']) }}</p>
-    @else
-        <h3 class="text-white font-semibold text-3xl text-center mb-5">{{$data['complete_title']}}</h3>
+     class="container mx-auto px-4 sm:px-6 lg:px-8 py-6 scroll-mt-[102px] relative z-10" id="{{$data['id']}}">
+    @if(in_array($user['status'], [\App\Concerns\Enums\Status::CONFIRMED->value]))
+        <h3 class="text-white font-semibold text-2xl text-center mb-3">How to  complete you registration?</h3>
+    @endif
+
+    @if(in_array($user['type'], [
+        \App\Concerns\Enums\Types::PARTICIPANT->value,
+        \App\Concerns\Enums\Types::STAFF->value,
+        \App\Concerns\Enums\Types::COMPANION->value,
+        \App\Concerns\Enums\Types::VIP->value,
+    ]))
+        @if(in_array($user['status'], [\App\Concerns\Enums\Status::UNPAID->value]))
+            <h3 class="text-white font-semibold text-2xl text-center mb-3">To continue the process please complete your payment</h3>
+      @endif
    @endif
+
+    @if(in_array($user['status'], [
+        \App\Concerns\Enums\Status::PENDING_APPROVAL_DATA->value,
+        \App\Concerns\Enums\Status::SEND_TO_CHANCELLERY->value,
+        \App\Concerns\Enums\Status::FINISHED->value,
+        \App\Concerns\Enums\Status::PENDING_CORRECT_DATA->value
+    ]))
+        <h3 class="text-white font-semibold text-3xl text-center mb-5">Want to use our complimentary shuttle service?</h3>
+        <p class="text-white text-center">Don’t forget to let us know your travel and accommodation arrangements</p>
+   @endif
+
     <div class="progress" :aria-valuenow="currentVal" :aria-valuemin="minVal" :aria-valuemax="maxVal">
         <div class="line" :style="`width: ${calcPercentage(minVal, maxVal, currentVal)}%; transition: 1s;`"></div>
     </div>
