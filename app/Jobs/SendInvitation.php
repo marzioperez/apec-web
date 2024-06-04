012ecca32fbf39312febcaaf4597e39dbbbe3ba0
Marzio Perez <marzioperez@gmail.com>
2024-06-03 22:40:11 -0500

    Update SendInvitation.php

--- app/Jobs/SendInvitation.php
+++ app/Jobs/SendInvitation.php
@@ -27,33 +27,30 @@ class SendInvitation implements ShouldQueue
     public function handle(): void {
         if ($this->data['email']) {
             $model = User::where('email', $this->data['email'])->first();
-            if ($model) {
-                Mail::to($this->data['email'])->send(new RegisterPassFree($model, $this->data['password']));
+            if (!$model) {
+                $new_password = Str::slug($this->data['password']);
+
+                $user = new User();
+                $user['code'] = GenerateCode::run($this->data['nombres'], $this->data['apellidos']);
+                $user['name'] = $this->data['nombres'];
+                $user['last_name'] = $this->data['apellidos'];
+                $user['business'] = $this->data['empresa'];
+                $user['role'] = $this->data['cargo'];
+                $user['email'] = $this->data['email'];
+                $user['attendee_email'] = $this->data['email_secundario'];
+                $user['phone'] = $this->data['telefono'];
+                $user['type'] = $this->data['tipo'];
+                $user['password'] = $new_password;
+                $user['companion_free'] = $this->data['acompanante_free'] === 'Si';
+                $user['companion_amount'] = $this->data['monto_acompanante'];
+                $user['amount'] = $this->data['monto_participante'];
+                $user['staff_free'] = $this->data['staffer_free'] === 'Si';;
+                $user['staff_amount'] = $this->data['monto_staffer'];
+                $user['status'] = Status::CONFIRMED->value;
+                $user->save();
+
+                Mail::to($this->data['email'])->send(new RegisterPassFree($user, $new_password));
             }
-//            if (!$model) {
-//                $new_password = Str::slug($this->data['password']);
-//
-//                $user = new User();
-//                $user['code'] = GenerateCode::run($this->data['nombres'], $this->data['apellidos']);
-//                $user['name'] = $this->data['nombres'];
-//                $user['last_name'] = $this->data['apellidos'];
-//                $user['business'] = $this->data['empresa'];
-//                $user['role'] = $this->data['cargo'];
-//                $user['email'] = $this->data['email'];
-//                $user['attendee_email'] = $this->data['email_secundario'];
-//                $user['phone'] = $this->data['telefono'];
-//                $user['type'] = $this->data['tipo'];
-//                $user['password'] = $new_password;
-//                $user['companion_free'] = $this->data['acompanante_free'] === 'Si';
-//                $user['companion_amount'] = $this->data['monto_acompanante'];
-//                $user['amount'] = $this->data['monto_participante'];
-//                $user['staff_free'] = $this->data['staffer_free'] === 'Si';;
-//                $user['staff_amount'] = $this->data['monto_staffer'];
-//                $user['status'] = Status::CONFIRMED->value;
-//                $user->save();
-//
-//
-//            }
         }
     }
 }
