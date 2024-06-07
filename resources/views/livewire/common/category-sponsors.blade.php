fcf9e524b818d865fbe7aa8e26625d1ec10770e5
Marzio Perez <marzioperez@gmail.com>
2024-06-06 19:32:04 -0500

    Update category-sponsors.blade.php

--- resources/views/livewire/common/category-sponsors.blade.php
+++ resources/views/livewire/common/category-sponsors.blade.php
@@ -1,4 +1,10 @@
-<div class="grid sm:grid-cols-center-5 grid-cols-center-2 sm:gap-12 gap-6">
+@php
+    $size = 5;
+    if (count($sponsors) === 1) {
+        $size = 4;
+    }
+@endphp
+<div class="grid sm:grid-cols-center-{{$size}} grid-cols-center-2 sm:gap-12 gap-6">
     @foreach($sponsors as $item)
         <div class="w-full cursor-pointer flex items-center" x-on:click.prevent="$dispatch('show-sponsor', {sponsor: {{json_encode($item)}} })">
             <img src="{{url('storage/web/' . $item['logo'])}}" class="object-center w-full" />
