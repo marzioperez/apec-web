ff49e92446f6663a0c2c3c83e39393c57e934d61
Marzio Perez <marzioperez@gmail.com>
2024-07-30 13:23:18 -0500

    Ajuste carrusel

--- resources/views/livewire/common/sponsors.blade.php
+++ resources/views/livewire/common/sponsors.blade.php
@@ -4,10 +4,46 @@
             <div class="col-span-full pb-5">
                 <h4 class="text-white font-semibold text-5xl uppercase">{{$data['title']}}</h4>
             </div>
-            <div class="col-span-full pb-5 flex justify-center space-x-3">
-                @foreach($categories as $c => $category)
-                    <button type="button" class="btn" x-on:click.prevent="category = {{$c}}" :class="category == {{$c}} ? 'btn-orange' : 'btn-orange-outline'">{{$category['name']}}</button>
-                @endforeach
+            <div class="col-span-full pb-5">
+                <div wire:ignore x-data="{
+                    init() {
+                        new Splide(this.$refs.category_sponsors, {
+                            perPage: 5,
+                            gap: 10,
+                            arrows: false,
+                            drag: false,
+                            pagination: false,
+                            destroy: true,
+                            breakpoints: {
+                                1024: {
+                                    perPage: 3,
+                                    drag: true,
+                                    pagination: true,
+                                    destroy: false
+                                },
+                                640: {
+                                    perPage: 2,
+                                    drag: true,
+                                    pagination: true,
+                                    destroy: false
+                                }
+
+                            }
+                        }).mount()
+                    }
+                }">
+                    <div x-ref="category_sponsors" class="splide sponsors-carousel">
+                        <div class="splide__track">
+                            <ul class="splide__list sm:!flex sm:justify-center sm:space-x-3">
+                                @foreach($categories as $c => $category)
+                                    <li class="splide__slide flex items-center justify-center">
+                                        <button type="button" class="btn" x-on:click.prevent="category = {{$c}}" :class="category == {{$c}} ? 'btn-orange' : 'btn-orange-outline'">{{$category['name']}}</button>
+                                    </li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    </div>
+                </div>
             </div>
             <div class="col-span-full py-6">
                 @foreach($categories as $c => $category)
