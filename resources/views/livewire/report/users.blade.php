1b680e909daf57c2541d3b3dc9ab294e8c7388b8
Marzio Perez <marzioperez@gmail.com>
2024-10-03 18:18:34 -0500

    Update users.blade.php

--- resources/views/livewire/report/users.blade.php
+++ resources/views/livewire/report/users.blade.php
@@ -72,7 +72,7 @@
 
             <div class="fi-form-actions mt-6">
                 <div class="fi-ac gap-3 flex flex-wrap items-center justify-start">
-                    <button wire:click.prevente="process" wire:target="process" wire:loading.attr="disabled"  style="--c-400:var(--primary-400);--c-500:var(--primary-500);--c-600:var(--primary-600); width: 100%;" class="fi-btn relative grid-flow-col items-center justify-center font-semibold outline-none transition duration-75 focus-visible:ring-2 rounded-lg fi-color-custom fi-btn-color-primary fi-color-primary fi-size-md fi-btn-size-md gap-1.5 px-3 py-2 text-sm inline-grid shadow-sm bg-custom-600 text-white hover:bg-custom-500 focus-visible:ring-custom-500/50 dark:bg-custom-500 dark:hover:bg-custom-400 dark:focus-visible:ring-custom-400/50 fi-ac-action fi-ac-btn-action" type="button">
+                    <button wire:click.prevent="process" wire:target="process" wire:loading.attr="disabled"  style="--c-400:var(--primary-400);--c-500:var(--primary-500);--c-600:var(--primary-600); width: 100%;" class="fi-btn relative grid-flow-col items-center justify-center font-semibold outline-none transition duration-75 focus-visible:ring-2 rounded-lg fi-color-custom fi-btn-color-primary fi-color-primary fi-size-md fi-btn-size-md gap-1.5 px-3 py-2 text-sm inline-grid shadow-sm bg-custom-600 text-white hover:bg-custom-500 focus-visible:ring-custom-500/50 dark:bg-custom-500 dark:hover:bg-custom-400 dark:focus-visible:ring-custom-400/50 fi-ac-action fi-ac-btn-action" type="button">
                         <span wire:loading.remove wire:target="process">Generar reporte</span>
                         <span wire:loading wire:target="process">Cargando...</span>
                     </button>
