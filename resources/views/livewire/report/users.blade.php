ac4e92c439a19487a0346b031c21e12cd3233b2d
Marzio Perez <marzioperez@gmail.com>
2024-10-03 18:13:49 -0500

    Update users.blade.php

--- resources/views/livewire/report/users.blade.php
+++ resources/views/livewire/report/users.blade.php
@@ -76,8 +76,9 @@
                         Generar reporte
                     </button>
                     @if(count($users) > 0)
-                        <button wire:click.prevent="export" style="--c-400:var(--success-400);--c-500:var(--success-500);--c-600:var(--success-600);" class="fi-btn relative grid-flow-col items-center justify-center font-semibold outline-none transition duration-75 focus-visible:ring-2 rounded-lg fi-color-custom fi-btn-color-primary fi-color-primary fi-size-md fi-btn-size-md gap-1.5 px-3 py-2 text-sm inline-grid shadow-sm bg-custom-600 text-white hover:bg-custom-500 focus-visible:ring-custom-500/50 dark:bg-custom-500 dark:hover:bg-custom-400 dark:focus-visible:ring-custom-400/50 fi-ac-action fi-ac-btn-action" type="button" wire:loading.attr="disabled" wire:click="export">
-                            Exportar en Excel
+                        <button wire:click.prevent="export" style="--c-400:var(--success-400);--c-500:var(--success-500);--c-600:var(--success-600);" wire:loading.attr="disabled" wire:target="export" class="fi-btn relative grid-flow-col items-center justify-center font-semibold outline-none transition duration-75 focus-visible:ring-2 rounded-lg fi-color-custom fi-btn-color-primary fi-color-primary fi-size-md fi-btn-size-md gap-1.5 px-3 py-2 text-sm inline-grid shadow-sm bg-custom-600 text-white hover:bg-custom-500 focus-visible:ring-custom-500/50 dark:bg-custom-500 dark:hover:bg-custom-400 dark:focus-visible:ring-custom-400/50 fi-ac-action fi-ac-btn-action" type="button" wire:loading.attr="disabled" wire:click="export">
+                            <span wire:loading.remove wire:target="export">Exportar en Excel</span>
+                            <span wire:loading wire:target="export">Cargando...</span>
                         </button>
                     @endif
                 </div>
